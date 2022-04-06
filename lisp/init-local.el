f543ae30eb315429f9928354fb1cf590ceb3cd2e
Dhruvin Shah <dhruvin3@gmail.com>
2022-04-05 20:00:43 -0400

    WSL specific setting from emacsredux

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -11,6 +11,23 @@
 (require-package 'zenburn-theme)
 (load-theme 'zenburn t)
 
+;;; most of my system right now are windows wsl system
+;;; windows 11 with native weston display or windows 10
+;;; with VcXsrv for wsl2 or wsl1. Few hack to such system
+;;; from
+;;; https://emacsredux.com/blog/2021/12/19/wsl-specific-emacs-configuration/
+(when (and (eq system-type 'gnu/linux)
+           (getenv "WSLENV"))
+
+  ;; Teach Emacs how to open links in your default Windows browser
+  (let ((cmd-exe "/mnt/c/Windows/System32/cmd.exe")
+        (cmd-args '("/c" "start")))
+    (when (file-exists-p cmd-exe)
+      (setq browse-url-generic-program  cmd-exe
+            browse-url-generic-args     cmd-args
+            browse-url-browser-function 'browse-url-generic
+            search-web-default-browser 'browse-url-generic))))
+
 (provide 'init-local)
 
 ;;; init-local.el ends here
